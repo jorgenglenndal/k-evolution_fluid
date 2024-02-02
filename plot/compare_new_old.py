c4f1b0a9e99779efffe9a816568cb1e7439660c5
Jørgen Glenndal <jorgeagl@uio.no>
2024-02-02 14:15:27 +0100

    ...

--- plot/compare_new_old.py
+++ plot/compare_new_old.py
@@ -48,6 +48,66 @@ def read_div_variables(file):
     return np.array(z),np.array(avg_pi),np.array(max_pi),np.array(avg_zeta),np.array(max_zeta), cs2_kessence,N_kessence 
 
 
+def plot(file,y,implementation,color,marker=False,s=False):
+    if marker != False and s != False:
+        z,avg_pi,max_pi,avg_zeta,max_zeta, cs2_kessence,N_kessence = read_div_variables(file) #read_div_variables("/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002/N1/div_variables.txt")
+
+        if y == "avg_pi":
+            plt.scatter(z,abs(avg_pi),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color,marker=marker,s=s)
+        elif y == "max_pi":
+            plt.scatter(z,abs(max_pi),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color,marker = marker,s=s)
+        elif y == "avg_zeta":
+            plt.scatter(z,abs(avg_zeta),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color,marker = marker,s=s)
+        elif y == "max_zeta":
+            plt.scatter(z,abs(max_zeta),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color,marker=marker, s=s)
+        else:
+            sys.exit(1)
+        #marker = marker
+        #s = s
+    else:
+        z,avg_pi,max_pi,avg_zeta,max_zeta, cs2_kessence,N_kessence = read_div_variables(file) #read_div_variables("/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002/N1/div_variables.txt")
+        #avg_pi = abs(avg_pi)
+        #avg_zeta = abs(avg_zeta)
+        #max_pi = abs(max_pi)
+        #max_zeta = abs(max_zeta)
+        if y == "avg_pi":
+            plt.scatter(z,abs(avg_pi),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color)
+        elif y == "max_pi":
+            plt.scatter(z,abs(max_pi),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color)
+        elif y == "avg_zeta":
+            plt.scatter(z,abs(avg_zeta),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color)
+        elif y == "max_zeta":
+            plt.scatter(z,abs(max_zeta),label= implementation+ " " + y + ", N_kess = " +str(int(N_kessence)),color=color)
+        else:
+            sys.exit(1)
+
+
+file = "/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002/N1/div_variables.txt"
+plot(file,"avg_pi","new","mediumblue")
+
+file = "/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002/N5/div_variables.txt"
+plot(file,"avg_pi","new","orange")
+
+file = "/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002/N10/div_variables.txt"
+plot(file,"avg_pi","new","seagreen")
+
+# old impl
+file = "/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002_test/N1/div_variables.txt"
+plot(file,"avg_pi","old","mediumblue",marker = "x",s=70)
+
+file = "/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002_test/N5/div_variables.txt"
+plot(file,"avg_pi","old","orange",marker = "x",s=70)
+
+file = "/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002_test/N10/div_variables.txt"
+plot(file,"avg_pi","old","seagreen",marker = "x",s=70)
+
+
+plt.gca().invert_xaxis()
+plt.yscale('log')
+plt.legend()
+plt.xlabel("z")
+plt.show()
+sys.exit(0)
 
 z,avg_pi,max_pi,avg_zeta,max_zeta, cs2_kessence,N_kessence = read_div_variables("/mn/stornext/d5/data/jorgeagl/kevolution_output/test/123002/N1/div_variables.txt")
 plt.scatter(z,abs(avg_pi),label="new avg_pi, " + "N_kess = " +str(int(N_kessence)),color="mediumblue")
@@ -74,7 +134,7 @@ plt.yscale('log')
 plt.legend()
 plt.xlabel("z")
 plt.show()
-sys.exit(0)
+#sys.exit(0)
 
 
 
